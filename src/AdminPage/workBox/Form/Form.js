fd3e90db0ecce03a2be4ef05655c318f2e261e9b
Yavuz Ege <egeokumus@gmail.com>
2019-12-06 22:23:58 +0300

    add headphone fixed

--- src/AdminPage/workBox/Form/Form.js
+++ src/AdminPage/workBox/Form/Form.js
@@ -6,6 +6,7 @@ import Label from './Label/Label';
 import axios from 'axios';
 import { DropdownList } from 'react-widgets';
 import 'react-widgets/dist/css/react-widgets.css';
+import { thisExpression } from '@babel/types';
 
 class Form extends Component {
 
@@ -17,7 +18,7 @@ class Form extends Component {
 
                 axios.post("https://awy9jz88zl.execute-api.us-east-2.amazonaws.com/test/", this.state.data, { crossdomain: true })
                         .then((response) => {
-                                console.log(response);
+                                alert(response.data);
                         })
 
         }
@@ -37,33 +38,46 @@ class Form extends Component {
                 data[i] = value;
                 this.setState({ data: data });
         }
-        
+        createComponent=(i,data,data_type)=>{
+                let options_dropdown = ['very bad', 'bad', 'average', 'good','very good'];
+                let options_soundtype = ['bass', 'balanced', 'v-shaped'];
+                let options_ear = ['in-ear', 'over-ear', 'on-ear'];
+                console.log("asd")
+                
+                                if(data_type==="input"){
+                                        return  (<InputField key={i} placeholder={data} changed={(e) => this.onInputFieldChangeHandler(i, e)}></InputField>);
+                                }
+                                else if(data_type==="soundtype"){
+                                        return (<DropdownList key={i} data={options_soundtype} onChange={(value)=>this.onDropDownChangeHandler(i,value)} placeholder={data}></DropdownList>)
+                                }
+                                else if(data_type==="ear"){
+                                        return (<DropdownList key={i}  data={options_ear} onChange={(value)=>this.onDropDownChangeHandler(i,value)} placeholder={data}></DropdownList>)
+                                }
+                                else {
+                                        return (<DropdownList key={i}  data={options_dropdown} onChange={(value)=>this.onDropDownChangeHandler(i,value)} placeholder={data}></DropdownList>)
+                                }
+
+                      
+        }
                 
 
         
         render() {
                
-                let options = ['very bad', 'bad', 'average', 'good','very good'];
-                console.log(this.props.data_type);
+                var element;
+
+                
+                
                 return (
                         
                         <div>
                                 
-                                {       
-                                       
-                                        this.props.data ? (this.props.data.map((item, i) =>
-
-                                                (      this.props.data_type[i]?
-                                                        (<InputField key={i} placeholder={item} changed={(e) => this.onInputFieldChangeHandler(i, e)}></InputField>)  :
-                                                        (<DropdownList data={options} onChange={(value)=>this.onDropDownChangeHandler(i,value)} placeholder={item}></DropdownList>)
-                                                        
-
-                                                )
-                                                
-                                        )) : null
-
-                                }
+                               {this.props.data.map((item,i)=>{
+                                       element=this.createComponent(i,item,this.props.data_type[i])
+                                       return element;
+                               })}
                                 <SubmitButton clicked={this.onSubmitButtonClickHandler}></SubmitButton>
+                                
                         </div>
 
                 )
