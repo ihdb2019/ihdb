35562a2a761473270de13f841e374191e97d96fe
Egehan Orta <egehanorta@gmail.com>
2019-12-22 15:35:21 +0300

    Functions created for news

--- src/AdminPage/AdminPage.js
+++ src/AdminPage/AdminPage.js
@@ -5,43 +5,59 @@ import Form from './workBox/Form/Form';
 import './AdminPage.css'
 
 
-function onAddHeadphoneButtonClickHandler(setAdding, setData, setDataType, adding) {
-    setAdding(!adding);
-    if (adding) {
+function onAddHeadphoneButtonClickHandler (addingHeadphone, setAddingHeadphone, editingHeadphone, setEditingHeadphone, addingNews, setAddingNews, editingNews, setEditingNews, setData, setDataType) {
+    setAddingHeadphone(!addingHeadphone);
+    if (addingNews) setAddingNews(!addingNews);
+    if (editingHeadphone) setEditingHeadphone(!editingHeadphone);
+    if (editingNews) setEditingNews(!editingNews);
+    if (addingHeadphone) {
         axios.get('https://r5j018428h.execute-api.us-east-2.amazonaws.com/test/getformheadphone', { crossdomain: true })
-
             .then(res => {
-
-
-
-
                 var data = [...res.data.body.fields];
                 var data_type = [...res.data.body.field_type];
-
                 setData(data);
                 setDataType(data_type);
             }
-            )
+        )
     }
 
 }
-function onListHeadphoneButtonClickHandler(setEditing, editing) {
-    setEditing(!editing);
+function onListHeadphoneButtonClickHandler(addingHeadphone, setAddingHeadphone, editingHeadphone, setEditingHeadphone, addingNews, setAddingNews, editingNews, setEditingNews) {
+    setEditingHeadphone(!editingHeadphone);
+    if (addingNews) setAddingNews(!addingNews);
+    if (addingHeadphone) setAddingHeadphone(!addingHeadphone);
+    if (editingNews) setEditingNews(!editingNews);
 
 
 }
-function onAddNewsButtonClickHandler() {
-    alert("am cocugu egehan2");
+function onAddNewsButtonClickHandler(addingHeadphone, setAddingHeadphone, editingHeadphone, setEditingHeadphone, addingNews, setAddingNews, editingNews, setEditingNews, setData) {
+    setAddingNews(!addingNews);
+    if (addingHeadphone) setAddingHeadphone(!addingHeadphone);
+    if (editingHeadphone) setEditingHeadphone(!editingHeadphone);
+    if (editingNews) setEditingNews(!editingNews);
+    if (addingNews) {
+        axios.get('https://v8wg906jm2.execute-api.us-east-2.amazonaws.com/new/getformnews', { crossdomain: true })
+            .then(res => {
+                var data = [...res.data.body.fields];
+                setData(data);
+            }
+        )
+    }
 
 }
-function onListNewsButtonClickHandler() {
-    alert("am cocugu egehan3");
+function onListNewsButtonClickHandler(addingHeadphone, setAddingHeadphone, editingHeadphone, setEditingHeadphone, addingNews, setAddingNews, editingNews, setEditingNews) {
+    setEditingNews(!editingNews);
+    if (addingNews) setAddingNews(!addingNews);
+    if (addingHeadphone) setAddingHeadphone(!addingHeadphone);
+    if (editingHeadphone) setEditingHeadphone(!editingHeadphone);
 
 }
 function AdminPage() {
 
-    const [adding, setAdding] = useState(false);
-    const [editing, setEditing] = useState(false);
+    const [addingHeadphone, setAddingHeadphone] = useState(false);
+    const [editingHeadphone, setEditingHeadphone] = useState(false);
+    const [addingNews, setAddingNews] = useState(false);
+    const [editingNews, setEditingNews] = useState(false);
     const [news, setNews] = useState(false);
     const [headphone, setHeadphone] = useState(false);
     const [nothing, setNothing] = useState(true);
@@ -52,20 +68,18 @@ function AdminPage() {
 
     return (
         <div className="container">
-
             <div className="box">
 
                 <div className="news">
-                    <AdminPageButton clicked={onAddNewsButtonClickHandler} >Add News</AdminPageButton>
-                    <AdminPageButton clicked={onListNewsButtonClickHandler} >List News</AdminPageButton>
+                    <AdminPageButton clicked={() => onAddNewsButtonClickHandler(addingHeadphone, setAddingHeadphone, editingHeadphone, setEditingHeadphone, addingNews, setAddingNews, editingNews, setEditingNews, setData)} >Add News</AdminPageButton>
+                    <AdminPageButton clicked={() => onListNewsButtonClickHandler(addingHeadphone, setAddingHeadphone, editingHeadphone, setEditingHeadphone, addingNews, setAddingNews, editingNews, setEditingNews)} >List News</AdminPageButton>
                 </div>
                 <div className="headphone">
-                    <AdminPageButton clicked={() => onAddHeadphoneButtonClickHandler(setAdding, setData, setDataType, adding)} >Add Headphone</AdminPageButton>
-
-                    <AdminPageButton clicked={() => onListHeadphoneButtonClickHandler(setEditing, editing)} >List Headphone</AdminPageButton>
+                    <AdminPageButton clicked={() => onAddHeadphoneButtonClickHandler(addingHeadphone, setAddingHeadphone, editingHeadphone, setEditingHeadphone, addingNews, setAddingNews, editingNews, setEditingNews, setData, setDataType)} >Add Headphone</AdminPageButton>
+                    <AdminPageButton clicked={() => onListHeadphoneButtonClickHandler(addingHeadphone, setAddingHeadphone, editingHeadphone, setEditingHeadphone, addingNews, setAddingNews, editingNews, setEditingNews)} >List Headphone</AdminPageButton>
                 </div>
 
-                <Form editing={editing} adding={adding} data={data} data_type={data_type}></Form>
+                <Form editingHeadphone={editingHeadphone} addingHeadphone={addingHeadphone} editingNews={editingNews} addingNews={addingNews} data={data} data_type={data_type}></Form>
             </div>
         </div>
     )
--- src/AdminPage/workBox/Form/Form.js
+++ src/AdminPage/workBox/Form/Form.js
@@ -17,48 +17,75 @@ class Form extends Component {
         state = {
                 data: [],
                 search: "",
-                editFormData:[],
-                editBox:false
+                editFormData: [],
+                editBox: false
         }
-        
-        onSubmitButtonClickHandler = () => {
+        onSubmitButtonClickHandler = (mode) => {
+                if (mode) {
+                        let data = {
+                                "WHOADDED" :this.state.data[0],
+                                "HEADPHONENAME":this.state.data[1],
+                                "HEADPHONEBRAND":this.state.data[2],
+                                "HEADPHONEREVIEW":this.state.data[3],
+                                "HEADPHONEPRICE":this.state.data[4],
+                                "SOUNDQUALITY":this.state.data[5],
+                                "SOUNDTYPE":this.state.data[6],
+                                "DURABILITY":this.state.data[7],
+                                "DRIVERCOUNT":this.state.data[8],
+                                "DRIVERTYPE":this.state.data[9],
+                                "EAR":this.state.data[10]
+                            }
+                        data=JSON.stringify(data);
+                        axios.post("https://awy9jz88zl.execute-api.us-east-2.amazonaws.com/test/",data, { crossdomain: true })
+                                .then((response) => {
+                                        alert(response.data);
+                                })
+                }
+                else{
+                        let data = {
+                                "WHOADDED" :this.state.data[0],
+                                "HEADER":this.state.data[1],
+                                "TEXT":this.state.data[2],
+                            }
+                        data=JSON.stringify(data);
+                        axios.post("https://v8wg906jm2.execute-api.us-east-2.amazonaws.com/new/addnews",data, { crossdomain: true })
+                                .then((response) => {
+                                        alert("AMCIK YAVUZ");
+                                })
+                }
 
-                axios.post("https://awy9jz88zl.execute-api.us-east-2.amazonaws.com/test/", this.state.data, { crossdomain: true })
-                        .then((response) => {
-                                alert(response.data);
+        }
+        onSearchSubmitButtonClickHandler = (mode) => {
+                var data2 = { 'searchstring': this.state.search }
+                var tmparray = [];
+                console.log(data2);
+                if(mode){
+                        axios.get("https://r5j018428h.execute-api.us-east-2.amazonaws.com/delirmetesti/getformheadphone", { params: data2 }, { crossdomain: true }).then((res) => {
+                                console.log(res)
+                                tmparray = [...res.data];
+                                this.setState({ editFormData: tmparray });
+                                this.setState({ editBox: true });
                         })
+                }
+                else{
+                        axios.get("https://v8wg906jm2.execute-api.us-east-2.amazonaws.com/test/searchnews", { params: data2}, { crossdomain: true }).then((res) => {
+                                console.log(res)
+                                //tmparray = [...res.data];
+                                //this.setState({ editFormData: tmparray });
+                                //this.setState({ editBox: true });
+                        })
+                }
 
-        }
-        onSearchSubmitButtonClickHandler = () => {
-                var data2= {'searchstring':this.state.search}
-                var tmparray=[];
-                
-                axios.get("https://r5j018428h.execute-api.us-east-2.amazonaws.com/delirmetesti/getformheadphone",{params:data2},{ crossdomain: true }).then((res) => {
-                        
-                        
-                        tmparray=[...res.data];
-                        
-                        this.setState({editFormData:tmparray});
-                        this.setState({editBox:true});
-                        
-                        
-                        
-                })
-                
         }
         onInputFieldChangeHandler = (i, e) => {
                 var data = [...this.state.data];
                 //data[i]=e.target.value;       
                 data[i] = e.target.value;
                 this.setState({ data: data });
-
-
-
-
         }
         onSearchFieldChange = (e) => {
-                this.setState({search:e.target.value});
-                
+                this.setState({ search: e.target.value });
+
         }
         onDropDownChangeHandler = (i, value) => {
                 var data = [...this.state.data];
@@ -66,7 +93,7 @@ class Form extends Component {
                 data[i] = value;
                 this.setState({ data: data });
         }
-        createComponent = (i, data, data_type) => {
+        createHeadphoneComponent = (i, data, data_type) => {
                 let options_dropdown = ['very bad', 'bad', 'average', 'good', 'very good'];
                 let options_soundtype = ['bass', 'balanced', 'v-shaped'];
                 let options_ear = ['in-ear', 'over-ear', 'on-ear'];
@@ -87,38 +114,60 @@ class Form extends Component {
 
 
         }
-     
+        createNewsComponent = (i, data) => {
+                return (<InputField key={i} placeholder={data} changed={(e) => this.onInputFieldChangeHandler(i, e)}></InputField>);
+        }
+
 
 
 
         render() {
 
                 var element;
-                var adding, editing;
-               
-                adding = this.props.adding;
-                editing = this.props.editing;
-                
-                
-                return (
-                        adding ? (
-                                <div>
+                var addingHeadphone, editingHeadphone, addingNews, editingNews;
+                addingHeadphone = this.props.addingHeadphone;
+                editingHeadphone = this.props.editingHeadphone;
+                addingNews = this.props.addingNews;
+                editingNews = this.props.editingNews;
 
+                return (
+                        addingHeadphone ? (
+                                <div>
                                         {this.props.data.map((item, i) => {
-                                                element = this.createComponent(i, item, this.props.data_type[i])
+                                                element = this.createHeadphoneComponent(i, item, this.props.data_type[i])
                                                 return element;
                                         })}
-                                        <SubmitButton clicked={this.onSubmitButtonClickHandler}></SubmitButton>
-
+                                        <SubmitButton clicked={() => this.onSubmitButtonClickHandler(true)}></SubmitButton>
                                 </div>
                         ) :
 
-                                editing ? (<div className="editform">
-                                        <SearchField changed={(e)=>this.onSearchFieldChange(e)} placeholder="Search"></SearchField>
-                                        <SubmitButton clicked={this.onSearchSubmitButtonClickHandler} ></SubmitButton>
-                                        <div>{this.state.editBox ? <EditBox data={this.state.editFormData}></EditBox>:null}
-                                       
-                                </div></div> ): null
+                        editingHeadphone ? (
+                                <div className="editform">
+                                        <SearchField changed={(e) => this.onSearchFieldChange(e)} placeholder="Search"></SearchField>
+                                        <SubmitButton clicked={() => this.onSearchSubmitButtonClickHandler(true)} ></SubmitButton>
+                                        <div>{this.state.editBox ? <EditBox data={this.state.editFormData}></EditBox> : null}
+                                        </div>
+                                </div>
+                        ) :
+                        addingNews ? (
+                                <div>
+                                        {this.props.data.map((item, i) => {
+                                                element = this.createNewsComponent(i, item)
+                                                return element;
+                                        })}
+                                        <SubmitButton clicked={() => this.onSubmitButtonClickHandler(false)}></SubmitButton>
+                                </div>
+                        ) :
+                        editingNews ? (
+                                <div className="editform">
+                                        <SearchField changed={(e) => this.onSearchFieldChange(e)} placeholder="Search"></SearchField>
+                                        <SubmitButton clicked={() => this.onSearchSubmitButtonClickHandler(false)} ></SubmitButton>
+                                        <div>{this.state.editBox ? <EditBox data={this.state.editFormData}></EditBox> : null}
+                                        </div>
+                                </div>
+                        ) : null
+
+                        
                 )
 
         }
